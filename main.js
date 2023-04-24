3f2de2b66edaa48e03f1ad7b61b93a3966ed6eef
runningTrutle2 <oscarmoren2010@gmail.com>
2023-04-24 15:11:30 -0700

    updated

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
+let myArray = [2, 3, 4];
+let x = myArray;
+
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    document.getElementById("button1").addEventListener("click", function () {
+    document.getElementById("output1").value=x[0]
+    document.getElementById("output2").value=x[1]
+    document.getElementById("output3").value=x[2]
 
-        document.getElementById("textbox1").value = "success";
+    
+    document.getElementById("cubed").addEventListener("click", function () {
+        let x = myArray.map(myCubed, myArray[i]); // check understanding
+
+        document.getElementById("output1").value=x[0]
+        document.getElementById("output2").value=x[1]
+        document.getElementById("output3").value=x[2]
     });
+
+    
+
+    
 });
 
